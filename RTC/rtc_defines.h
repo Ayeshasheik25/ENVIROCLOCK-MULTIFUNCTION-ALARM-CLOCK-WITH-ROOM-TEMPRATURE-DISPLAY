a3705df3e055ae1b31202153542164f7de977a23
Ayeshasheik25 <ayeshasheik509@gmail.com>
2025-12-03 11:05:43 +0530

    Add files via upload

---
+++ RTC/rtc_defines.h
@@ -0,0 +1,13 @@
+#ifndef	RTC_DEFINES_H
+#define RTC_DEFINES_H
+//system clock and peripheral clock macros
+#define FOSC 12000000
+#define CCLK (5*FOSC)
+#define PCLK (CCLK/4)
+//RTC MACROS
+#define PREINT_VAL ((int)((PCLK/32768)-1))
+#define PREFRAC_VAL (PCLK-((PREINT_VAL+1)*32768))
+
+#define RTC_ENABLE (1<<0)
+#define RTC_RESET (1<<1)
+#endif
